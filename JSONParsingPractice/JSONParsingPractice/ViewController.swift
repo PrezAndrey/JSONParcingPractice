2dcfaa10c0063a098f6bdca2477a0ce4d6b39c59
PrezAndrey <aw06.1994@yandex.ru>
2022-08-25 17:04:42 +0300

    TableView implemented

--- JSONParsingPractice/JSONParsingPractice/ViewController.swift
+++ JSONParsingPractice/JSONParsingPractice/ViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupTableView()
+    }
+    
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Bar Baz Foo"
+        
+        return cell
     }
 
 
